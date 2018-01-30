f97d067e2cbcc4dee20d7fd9bf56502cff580902
Elenadie <harmuncolacao@hotmail.com>
2018-01-30 16:39:59 +0100

    4.3 custom controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
   resources :products
+
   get 'simple_pages/about'
 
   get 'simple_pages/contact'
 
-  root 'simple_pages#landing_page'
+ root 'simple_pages#landing_page'
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
