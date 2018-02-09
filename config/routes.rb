3546b7f93a8e1415248eb83537810ed60b05ee1f
Elenadie <harmuncolacao@hotmail.com>
2018-02-09 12:59:05 +0100

    no changes added

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
+  get '/products/:id', to: 'products#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
