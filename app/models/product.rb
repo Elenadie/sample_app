d1fa10c86f9690261d6c82268aee053b2ecf952e
Elenadie <harmuncolacao@hotmail.com>
2018-02-14 20:00:12 +0100

    5.2 forms

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,11 @@
 class Product < ApplicationRecord
   has_many :orders
-end
 
 def self.search(search_term)
-  if search
-      find(:all, :conditions => ["style ILIKE ? OR construction ILIKE ?", "%#{search}%", "%#{search}%"])
+  if search_term
+      find(:all, :conditions => ["style ILIKE ? OR construction ILIKE ?", "%#{search_term}%", "%#{search_term}%"])
     else
       find(:all)
     end
+  end
 end
