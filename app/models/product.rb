5d1c9e71201299dc6fb6ef8a82fab687252bde62
Elenadie <harmuncolacao@hotmail.com>
2018-02-15 11:38:28 +0100

    5.2 forms

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,10 @@ class Product < ApplicationRecord
   has_many :orders
 
   def self.search(search_term)
-    Product.where("name LIKE ?", "%#{search_term}%")
+    if Rails.env.production?
+      Product.where("name ilike ?", "%#{search_term}%") | Product.where("description ilike ?", "%#{search_term}%")
+    else
+      Product.where("name LIKE ?", "%#{search_term}%") | Product.where("description LIKE ?", "%#{search_term}%")
+    end
   end
 end
