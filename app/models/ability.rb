5f0bf9e5cc49e1452a103729ba2e6ace40751bfd
Elenadie <harmuncolacao@hotmail.com>
2018-02-27 12:59:04 +0100

    5.10 admin role and moderating users comment

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,14 @@ class Ability
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
 
+    def initialize(user)
+      if user.admin?
+        can :destroy, :comments
+      else
+        can :read, :all
+      end
+    end
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
