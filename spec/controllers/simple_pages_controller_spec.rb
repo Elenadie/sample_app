3c73b68e116eda48a53edfd84c81fd866f8f36e7
Elenadie <harmuncolacao@hotmail.com>
2018-03-05 20:18:46 +0100

    6.3 functional tests

--- spec/controllers/simple_pages_controller_spec.rb
+++ spec/controllers/simple_pages_controller_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
-describe MyController do
-  login_admin
+login_admin
 
-  it "should have a current_user" do
-    # note the fact that you should remove the "validate_session" parameter if this was a scaffold-generated controller
-    expect(subject.current_user).to_not eq(nil)
-  end
+it "should have a current_user" do
+  # note the fact that you should remove the "validate_session" parameter if this was a scaffold-generated controller
+  expect(subject.current_user).to_not eq(nil)
+end
 
-  it "should get index" do
-    # Note, rails 3.x scaffolding may add lines like get :index, {}, valid_session
-    # the valid_session overrides the devise login. Remove the valid_session from your specs
-    get 'index'
-    response.should be_success
-   end
-  end
+it "should get index" do
+  # Note, rails 3.x scaffolding may add lines like get :index, {}, valid_session
+  # the valid_session overrides the devise login. Remove the valid_session from your specs
+  get 'index'
+  response.should be_success
+ end
+
+
 
 describe SimplePagesController, type: :controller do
   context 'GET #index' do
@@ -26,4 +26,4 @@ describe SimplePagesController, type: :controller do
   end
 
 
-end
+  end
