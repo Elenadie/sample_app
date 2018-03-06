7267eb0dade11105c5e76540dff742ec47dd22ea
Elenadie <harmuncolacao@hotmail.com>
2018-03-07 00:07:15 +0100

    6.4 generating test data

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,18 +9,18 @@ describe UsersController, type: :controller do
  describe 'GET #show' do
       context 'when a user is logged in' do
         before do
-          sign_in @user
+          sign_in @user = FactoryBot.create(:user)
         end
         it 'loads correct user details' do
-             get :show, params: { id: user.id }
+             get :show, params: { id: @user = FactoryBot.create(:user).id }
              expect(response).to be_ok
-             expect(assigns(:user)).to eq @user
+             expect(assigns(@user = FactoryBot.create(:user))).to eq @user = FactoryBot.create(:user)
           end
         end
 
       context 'when a user is not logged in' do
         it 'redirects to login' do
-          get :show, params: { id: user.id }
+          get :show, params: { id: @user = FactoryBot.create(:user).id }
           expect(response).to redirect_to(new_user_session_path)
        end
     end
