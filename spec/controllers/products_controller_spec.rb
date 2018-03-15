9680f005f4836d52a946aa52c08ab2ee1f069e62
Elenadie <harmuncolacao@hotmail.com>
2018-03-15 13:06:46 +0100

    6.15 Production-ready web app

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,3 +1,65 @@
 require 'rails_helper'
 
 describe ProductsController, type: :controller do
+  context 'GET #index' do
+    it 'renders the index template' do
+      get :index
+      expect(response).to be_ok
+      expect(response).to render_template('index')
+    end
+  end
+
+   context 'GET #show' do
+     it 'renders the show template' do
+       get :show
+       expect(response).to be_ok
+       expect(response).to render_template('show')
+     end
+   end
+
+   context 'GET #new' do
+     it 'renders the new template' do
+       get :new
+       expect(response).to be_ok
+       expect(response).to render_template('new')
+     end
+   end
+
+   context 'GET #edit' do
+     it 'renders the edit template' do
+       get :new
+       expect(response).to be_ok
+       expect(response).to render_template('edit')
+     end
+   end
+
+   describe 'GET #create' do
+     context 'when a product is saved' do
+       expect(response).to redirect_to(products_path)
+      end
+
+     context 'when a product is not saved' do
+       expect(response).to render_template(create)
+
+     end
+   end 
+
+   describe 'GET #update' do
+     context 'when a product is updated' do
+       expect(response).to redirect_to(products_path)
+      end
+
+     context 'when a product is not updated' do
+       expect(response).to render_template(update)
+    end
+   end
+
+   context 'GET #destroy' do
+     it 'renders the destroy template' do
+       get :new
+       expect(response).to be_ok
+       expect(response).to render_template('destroy')
+     end
+   end
+
+ end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,14 @@
 require "rails_helper"
 
 describe Comment, type: :model do
+  context "rating must be present" do
 
+    let (:product) { Product.create!(name: "Momo")}
+
+    let (:comment) { User.create!(rating:"3")}
+
+   it "is not valid without a rating" do
+     expect(Comment.new(rating: "")).not_to be_valid
+   end
+  end
 end
